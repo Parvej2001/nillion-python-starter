796c14cd677074e0c50f6b9b293cb7781e1047b0
Parvej2001 <shaikhp055@gmail.com>
2024-07-03 05:49:39 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,12 +1,25 @@
 from nada_dsl import *
 
-
 def nada_main():
+    # Define parties
     party1 = Party(name="Party1")
-    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+    party2 = Party(name="Party2")
 
-    # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # make sure you change the output below to be your new output
+    # Define secret integer inputs for each party
+    my_int1_party1 = SecretInteger(Input(name="my_int1", party=party1))
+    my_int2_party1 = SecretInteger(Input(name="my_int2", party=party1))
 
-    return [Output(my_int1, "my_output", party1)]
+    my_int1_party2 = SecretInteger(Input(name="my_int1", party=party2))
+    my_int2_party2 = SecretInteger(Input(name="my_int2", party=party2))
+
+    # Perform some computation involving the inputs
+    sum_party1 = my_int1_party1 + my_int2_party1
+    product_party2 = my_int1_party2 * my_int2_party2
+
+    # Output the results for each party
+    outputs = [
+        Output(sum_party1, "sum_output", party1),
+        Output(product_party2, "product_output", party2)
+    ]
+
+    return outputs
